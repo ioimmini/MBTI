7c8674a5944538f434e3611960f7bc9c9284cc42
ioimmini <jumin0114@gmail.com>
2023-08-26 15:49:39 +0900

    MBTI 사이트

--- start/js/start.js
+++ start/js/start.js
@@ -1,10 +1,35 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
+
 const endPoint = 12;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+function calResult() {
+   console.log(select);
+    var result = select.indexOf(Math.max(...select));
+    return result;
+}
+    
+function setResult(){
+    let point = calResult();
+    const resultName = document.querySelector('.resultname');
+    resultName.innerHTML = infoList[point].name;
 
-function goResult(){
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult() {
     qna.style.WebgitAnimation = "fadeOut 1s";
     qna.style.animation = "fadeOut 1s";
     setTimeout(() => {
@@ -12,11 +37,15 @@ function goResult(){
         result.style.animation = "fadeIn 1s";
         setTimeout(() => {
             qna.style.display = "none";
-            result.style.display="block";
-        }, 450)})
+            result.style.display = "block";
+        }, 450)
+    })
+
+    setResult();
+    calResult();
 }
 
-function addAnswer(answerText, qIdx){
+function addAnswer(answerText, qIdx, idx) {
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
     answer.classList.add('answerList');
@@ -28,38 +57,43 @@ function addAnswer(answerText, qIdx){
     a.appendChild(answer);
     answer.innerHTML = answerText;
 
-    answer.addEventListener("click", function(){
+    answer.addEventListener("click", function () {
         var children = document.querySelectorAll('.answerList');
-        for(let i = 0; i < children.length; i++){                            
+        for (let i = 0; i < children.length; i++) {
             children[i].disabled = true;
             children[i].style.WebgitAnimation = "fadeOut 0.5s";
             children[i].style.animation = "fadeOut 0.5s";
         }
-        setTimeout(()=> {
-            for(let i = 0; i < children.length; i++){
+        setTimeout(() => {
+            var target = qnaList[qIdx].a[idx].type;
+            for (let i = 0; i < target.length; i++) {
+                select[target[i]] += 1;
+            }
+
+            for (let i = 0; i < children.length; i++) {
                 children[i].style.display = 'none';
             }
             goNext(++qIdx);
-        },450)
+        }, 450)
     }, false);
 }
 
-function goNext(qIdx){
-    if(qIdx+1 === endPoint){
+function goNext(qIdx) {
+    if (qIdx === endPoint) {
         goResult();
-        return ;
+        return;
     }
-    
-    var q =document.querySelector('.qBox');
+
+    var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q;
-    for(let i in qnaList[qIdx].a){
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    for (let i in qnaList[qIdx].a) {
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
-    var status = document.querySelector('.statusBar');  
-    status.style.width = (100/endPoint) * (qIdx+1) + '%';
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
 }
 
-function begin(){
+function begin() {
     main.style.WebgitAnimation = "fadeOut 1s";
     main.style.animation = "fadeOut 1s";
     setTimeout(() => {
@@ -67,7 +101,7 @@ function begin(){
         qna.style.animation = "fadeIn 1s";
         setTimeout(() => {
             main.style.display = "none";
-            qna.style.display="block";
+            qna.style.display = "block";
         }, 450)
         let qIdx = 0;
         goNext(qIdx);
