834b92b170e383c6a2a17915ba98ba9bf76984b6
ioimmini <jumin0114@gmail.com>
2023-09-20 14:23:41 +0900

    Disney Princess

--- start/js/share.js
+++ start/js/share.js
@@ -1,16 +1,24 @@
+const url = 'https://ioimmini-mbti-site.netlify.app/';
 
-function shareMessage() {
+function setShare(){
+    var resultImg = document.querySelector("#resultImg");
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = 'Diseny Princess';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+    
     Kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
-            title: '딸기 치즈 케익',
-            description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+            title: shareTitle,
+            description: shareDes,
             imageUrl:
-                'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+                shareImage,
             link: {
                 // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-                mobileWebUrl: 'https://developers.kakao.com',
-                webUrl: 'https://developers.kakao.com',
+                mobileWebUrl: shareURL,
+                webUrl: shareURL,
             },
         },
         social: {
@@ -20,17 +28,10 @@ function shareMessage() {
         },
         buttons: [
             {
-                title: '웹으로 보기',
+                title: '결과 확인하기',
                 link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
-                },
-            },
-            {
-                title: '앱으로 보기',
-                link: {
-                    mobileWebUrl: 'https://developers.kakao.com',
-                    webUrl: 'https://developers.kakao.com',
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
                 },
             },
         ],
